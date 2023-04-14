f1889e903ab9de8eaa4f33278922c1151b0168a1
K3yJey <kevin_madrid10@hotmail.es>
2023-04-14 11:00:25 -0500

    style: :lipstick: Se cambió la estructura del proyecto

--- script/clienteBanco.js
+++ script/clienteBanco.js
@@ -1,9 +1,9 @@
 export class clienteBanco {
-    nombrecliente;
-    dniCliente;
+  nombrecliente;
+  dniCliente;
 
-    constructor(nombrecliente, dniCliente) {
-        this.nombrecliente = nombrecliente;
-        this.dniCliente = dniCliente;
-    }
+  constructor(nombrecliente, dniCliente) {
+    this.nombrecliente = nombrecliente;
+    this.dniCliente = dniCliente;
+  }
 }
